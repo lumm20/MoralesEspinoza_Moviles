557a044c996e0ecf1255c459fee9bb9196b7d11c
luisa <moraLF20@outlook.com>
2025-03-24 08:18:44 -0700

    practica de autenticacion

---
+++ PracticaAutenticacionMoralesL/app/src/main/java/mx/itson/moviles/practicaautenticacionmoralesl/LoginActivity.kt
@@ -0,0 +1,76 @@
+package mx.itson.moviles.practicaautenticacionmoralesl
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        val email: EditText =findViewById(R.id.etEmail)
+        val password: EditText =findViewById(R.id.etPassword)
+        val errorTv: TextView =findViewById(R.id.tvError)
+        val button: Button =findViewById(R.id.btnRegister)
+
+        errorTv.visibility = View.INVISIBLE
+        button.setOnClickListener({
+            login(email.text.toString(),password.text.toString())
+        })
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun gotoMain(user: FirebaseUser){
+        val intent=Intent(this,MainActivity::class.java)
+        intent.putExtra("user",user.email)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    fun showError(text:String = "",visible:Boolean){
+        val errorTv :TextView=findViewById(R.id.tvError)
+        errorTv.text=text
+        errorTv.visibility = if(visible) View.VISIBLE else View.INVISIBLE
+    }
+
+    public override fun onStart(){
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser!=null){
+            gotoMain(currentUser)
+        }
+    }
+
+    fun login(email:String,pass:String){
+        auth.signInWithEmailAndPassword(email,pass).
+                addOnCompleteListener(this){task ->
+                    if(task.isSuccessful){
+                        val user = auth.currentUser
+                        showError(visible = false)
+                        gotoMain(user!!)
+                    }else{
+                        showError("Usuario y/o contraseña equivocados",true)
+                    }
+                }
+    }
+}
---
+++ PracticaAutenticacionMoralesL/app/src/main/java/mx/itson/moviles/practicaautenticacionmoralesl/SignInActivity.kt
@@ -0,0 +1,77 @@
+package mx.itson.moviles.practicaautenticacionmoralesl
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+
+        auth =Firebase.auth
+
+        val email:EditText=findViewById(R.id.etEmail)
+        val password:EditText=findViewById(R.id.etPassword)
+        val confPass:EditText=findViewById(R.id.etConfirmPassword)
+        val errorTv:TextView=findViewById(R.id.tvError)
+        val button:Button=findViewById(R.id.btnRegister)
+
+        errorTv.visibility= View.INVISIBLE
+
+        button.setOnClickListener({
+            if(email.text.isEmpty() || password.text.isEmpty() || confPass.text.isEmpty()){
+                errorTv.text = "Todos los campos deben de ser llenados"
+                errorTv.visibility = View.VISIBLE
+            }else if(!password.text.toString().equals(confPass.text.toString())){
+                errorTv.text = "Las contraseñas no coinciden"
+                errorTv.visibility = View.VISIBLE
+            }else{
+                errorTv.visibility = View.INVISIBLE
+                signIn(email.text.toString(),password.text.toString())
+            }
+        })
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun signIn(email:String,pass:String){
+        Log.d("INFO","email: ${email}, password: ${pass}")
+        auth.createUserWithEmailAndPassword(email,pass).
+                addOnCompleteListener(this){task->
+                    if(task.isSuccessful){
+                        Log.d("INFO","signInWithEmail:success")
+                        val user=auth.currentUser
+                        val intent=Intent(this,MainActivity::class.java)
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                    }else{
+                        Log.w("ERROR","signInWithEmail:failure",task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "El registro fallo",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+    }
+
+}
